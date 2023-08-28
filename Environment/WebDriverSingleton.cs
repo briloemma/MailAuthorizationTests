0fa8de07cff8a68a301effb9597c047a7e931af6
Эмма Брило <briloemma@gmail.com>
2023-08-28 19:57:46 +0300

    changes to chrome options

--- Environment/WebDriverSingleton.cs
+++ Environment/WebDriverSingleton.cs
@@ -18,6 +18,7 @@ namespace MailAuthorizationTests.Environment
                 new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.Latest);
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions.AddArgument("enable-automation");
+                chromeOptions.AddArgument("--headless");
                 chromeOptions.AddArgument("--window-size=2560,1600");
                 chromeOptions.AddArgument("--no-sandbox");
                 chromeOptions.AddArgument("--disable-extensions");
